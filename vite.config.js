380c202da2803057b1773db86c00107e1e4e5ec1
Mateusz Bielak <mt.bielak@gmail.com>
2023-06-08 08:47:27 +0200

    Add setupVitest

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
-import viteTsconfigPaths from "vite-tsconfig-paths";
-import vitestFetchMock from "vitest-fetch-mock";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(() => ({
-  build: { outDir: "build" },
-  plugins: [react(), eslint(), viteTsconfigPaths(), vitestFetchMock()],
+  build: { outDir: 'build' },
+  plugins: [react(), eslint(), viteTsconfigPaths()],
+  test: {
+    setupFiles: ['./setupVitest.ts'],
+  },
 }));
